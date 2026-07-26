af93024979e916c3c150f1c4828f5cd858f7aed1
Brandon <serrebi101@gmail.com>
2026-07-26 14:28:02 -0700

    Trigger verified v1.3.0 Windows release

--- requestcast/__init__.py
+++ requestcast/__init__.py
@@ -1,3 +1,4 @@
 """RequestCast: search, download, tag, and file music into a request library."""
 
 __version__ = "1.3.0"
+# Changing this file intentionally triggers the versioned Windows release workflow.
